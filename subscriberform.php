871fbf881b76f8e9c7c8d93d15e4f1050c9604f1
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-07-18 10:48:44 -0300

    Testando email form

---
+++ subscriberform.php
@@ -0,0 +1,15 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("miltonrodrigues713@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+?>
